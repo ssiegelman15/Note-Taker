d5a2d2a9fa01d8c81bc090af4df2b172daf60516
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-01 09:19:17 -0600

    added GET for /notes

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App is listening at http://localhost:${PORT}`)
 );
