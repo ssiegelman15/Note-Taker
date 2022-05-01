23c9e0f565f591a133c3d2c57bb01d26cf02b563
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-01 08:27:21 -0600

    create routes folder with notes.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+
 // Set up port so it can be tested on Heroku
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
 // Set up Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
