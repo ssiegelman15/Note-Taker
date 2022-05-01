c996aa1d62b1f487b78e3b48648c877c84aeab54
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-01 14:28:58 -0600

    first stab at writing app.delete details

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Do I need to have an app.get for '/' or does the express.static take care of that?
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -45,10 +46,19 @@ app.post('/api/notes', (req,res) => {
   }
 });
 
-app.delete('/api/notes/:id', (req,res) => 
+app.delete('/api/notes/:id', (req,res) => {
   // see activity for looping through json file items
-  res.send('Got a DELETE request at /api/notes/:id')
-);
+  // res.send('Got a DELETE request at /api/notes/:id')
+  if (req.params.id) {
+  readFromFile('./db/db.json').then((result) => {
+    let parsedDb = JSON.parse(result);
+    finalParsed = parsedDb.filter(note => note.id !== result.params.id);
+    writeToFile('./db/db.json', finalParsed);
+    res.json(finalParsed)
+  })} else {
+    res.json("Sorry, there was an issue deleting your note");
+  }
+});
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
