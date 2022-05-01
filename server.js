5bf7666657a66fb7515894fb0903e541a418323c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-01 08:21:25 -0600

    added port and some middleware

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-// Set-up port so it can be tested on Heroku
+// Set up port so it can be tested on Heroku
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+// Set up Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
