ba8dfe3096b7d2068cd42fe47be35a9c54f72481
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-01 20:17:31 -0600

    Added comments to explain my code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+// Set helpers constants (copied from class activites) that should help
 const uuid = require('./helpers/uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('./helpers/fsUtils');
 
@@ -12,6 +13,7 @@ const db = require('./db/db.json');
 // Set up port so it can be tested on Heroku
 const PORT = process.env.PORT || 3001;
 
+// Initialize express
 const app = express();
 
 // Set up Middleware
@@ -20,21 +22,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Do I need to have an app.get for '/' or does the express.static take care of that?
+// Create GET for initial page load without any additional path specified
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
+// Return notes.html file at /notes path
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// Create GET api for the notes in the database file
 app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((notes) =>
     res.json(JSON.parse(notes)));
 });
 
+// Create post to take care of saving notes when the save icon is clicked
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
+  // Check for a request.body to exist and if it does, save the note. Otherwise, return an error response
   if (req.body) {
     const note = {
       title,
@@ -48,6 +55,8 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
+// Deletes note when clicking on the trash icon by rewriting the database with all the same notes except for the one whose icon was clicked
+// by using the randomly assigned ID to make sure it's the only one deleted
 app.delete('/api/notes/:id', (req, res) => {
   if (req.params.id) {
     readFromFile('./db/db.json').then((result) => {
@@ -61,13 +70,12 @@ app.delete('/api/notes/:id', (req, res) => {
   }
 });
 
+// Return the index.html file for any path not defined in this file
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+// Set up the Port used by Express
 app.listen(PORT, () =>
   console.log(`App is listening at http://localhost:${PORT}`)
 );
-
-
-// Notes that are saved/deleted remain shown on the page until I kill the server and restart
